6d42f5c4f6289cfa5a8a505f07c223935e80a797
Song jae hyeok <wakij6587@naver.com>
2021-01-14 12:30:29 +0900

    클릭시 fullscreen 효과 적용 및 1초 후 링크 이동
    클릭시 fullscreen 효과 적용 및 1초 후 링크 이동

--- script/main.js
+++ script/main.js
@@ -2,9 +2,11 @@
 
 
 const title_list = $("li.title_list");
+const title_link = $("li.title_list > a");
 const title_txt = $("img.title_txt");
 const title_img = $("div.title_img");
 const title_tool = $("span.title_tool");
+const order_wrap = $("div.order_wrap");
 const order_count = $("span.order_txt > span");
 let cur_title = 0;
 
@@ -16,7 +18,6 @@ function btn_down(){
     cur_title++;
     btn_click();
 }
-
 function btn_click(){
     if(cur_title > 3){
         cur_title = 0;
@@ -27,4 +28,16 @@ function btn_click(){
     title_list.removeClass("active_project");
     title_list.eq(cur_title).addClass("active_project");
     order_count.html(cur_title + 1);
+}
+
+title_link.click(click_link);
+
+function click_link(e){
+    e.preventDefault();
+    const el = e.currentTarget;
+    title_list.eq(cur_title).addClass("click_project");
+    order_wrap.addClass("click_project");
+    setTimeout(()=>{
+        location.href = el.getAttribute("href");
+    }, 1000)
 }
