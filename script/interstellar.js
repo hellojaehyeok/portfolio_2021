d7bda20bc6ae12fca08839b03c4876c2502d6a62
Song jae hyeok <wakij6587@naver.com>
2021-01-15 10:50:12 +0900

    인터스텔라 자바스크립트 수정(가로 드래그)
    인터스텔라 자바스크립트 수정(가로 드래그)

--- script/interstellar.js
+++ script/interstellar.js
@@ -3,9 +3,10 @@
 const slider = $("ul.slider");
 let is_down = false;
 let startX;
-let scrollLeft;
+let scrollLeft = slider.offset().left;
+let scrollLeft_init = slider.offset().left;
 
-
+console.log(slider.offset().left);
 slider.mousedown(slider_down);
 slider.mouseleave(slider_leave);
 slider.mouseup(slider_up);
@@ -13,7 +14,7 @@ slider.mousemove(slider_move);
 
 function slider_down(e){
     is_down = true;
-    startX = e.pageX - slider.offset().left; 
+    startX = e.pageX - slider.offset().left;
     scrollLeft = slider.offset().left;
 }
 function slider_leave(){
@@ -23,11 +24,12 @@ function slider_up(){
     is_down = false;
 }
 function slider_move(e){
-    // if(!is_down)return;
+    if(!is_down)return;
     e.preventDefault();
     const x = e.pageX - slider.offset().left;
     const walk = x - startX;
-
     slider.css("left", scrollLeft + walk + "px");
-    console.log(walk);
+    if(scrollLeft_init <= slider.offset().left){
+        slider.css("left", scrollLeft_init + "px");
+    }
 }
